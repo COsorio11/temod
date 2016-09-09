aa2aff54603742d5f3bf89db4a2bc5ba90027259
localadmin <dbayl@allstate.com>
2016-09-09 08:37:47 -0700

    checkpoint two sons of crackers

--- traffic.go
+++ traffic.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"fmt"
+	"image/color"
+	"log"
+
 	"engo.io/ecs"
 	"engo.io/engo"
 	"engo.io/engo/common"
@@ -14,7 +18,21 @@ type City struct {
 	common.SpaceComponent
 }
 
-// Type uniquely defines your game type
+type CityBuildingSystem struct{}
+
+func (*CityBuildingSystem) Remove(ecs.BasicEntity) {}
+
+func (*CityBuildingSystem) Update(dt float32) {
+	if engo.Input.Button("AddCity").JustPressed() {
+		fmt.Println("The gamer pressed F1")
+	}
+}
+
+func (*CityBuildingSystem) New(*ecs.World) {
+	fmt.Println("CityBuildingSystem was added to the Scene")
+}
+
+// Type uniquely defines your game type37G
 func (*myScene) Type() string { return "myGame" }
 
 // Preload is called before loading any assets from the disk,
@@ -26,7 +44,35 @@ func (*myScene) Preload() {
 // Setup is called before the main loop starts. It allows you
 // to add entities and systems to your Scene.
 func (*myScene) Setup(world *ecs.World) {
+	engo.Input.RegisterButton("AddCity", engo.F1)
+	common.SetBackground(color.White)
 	world.AddSystem(&common.RenderSystem{})
+	world.AddSystem(&CityBuildingSystem{})
+
+	city := City{BasicEntity: ecs.NewBasic()}
+
+	city.SpaceComponent = common.SpaceComponent{
+		Position: engo.Point{10, 10},
+		Width:    303,
+		Height:   641,
+	}
+
+	texture, err := common.LoadedSprite("textures/city.png")
+	if err != nil {
+		log.Println("Unable to load texture: " + err.Error())
+	}
+
+	city.RenderComponent = common.RenderComponent{
+		Drawable: texture,
+		Scale:    engo.Point{0.5, 0.5},
+	}
+
+	for _, system := range world.Systems() {
+		switch sys := system.(type) {
+		case *common.RenderSystem:
+			sys.Add(&city.BasicEntity, &city.RenderComponent, &city.SpaceComponent)
+		}
+	}
 }
 
 func main() {
