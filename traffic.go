e88561ae22e3c173625cfadbce26f82ac914f69e
localadmin <dbayl@allstate.com>
2016-09-09 08:10:08 -0700

    first checkpoint

---
+++ traffic.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"engo.io/ecs"
+	"engo.io/engo"
+	"engo.io/engo/common"
+)
+
+type myScene struct{}
+
+type City struct {
+	ecs.BasicEntity
+	common.RenderComponent
+	common.SpaceComponent
+}
+
+// Type uniquely defines your game type
+func (*myScene) Type() string { return "myGame" }
+
+// Preload is called before loading any assets from the disk,
+// to allow you to register / queue them
+func (*myScene) Preload() {
+	engo.Files.Load("textures/city.png")
+}
+
+// Setup is called before the main loop starts. It allows you
+// to add entities and systems to your Scene.
+func (*myScene) Setup(world *ecs.World) {
+	world.AddSystem(&common.RenderSystem{})
+}
+
+func main() {
+	opts := engo.RunOptions{
+		Title:  "Hello World",
+		Width:  400,
+		Height: 400,
+	}
+	engo.Run(opts, &myScene{})
+}
